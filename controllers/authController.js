b7043e7e3eed248fe0342aa64653f6c4b15da279
Dorian-Cesar <dorian.celu@gmail.com>
2025-11-14 11:46:32 -0300

    less passw of login

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,13 +12,21 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  //const user = await User.findOne({ email });
+
+  const usershow = await User.findOne({ email }).select("_id name email role");
+
+  const user = await User.findOne({ email }).select("+password");
   if (!user || !(await user.comparePassword(password)))
     return res.status(401).json({ message: "Credenciales inválidas" });
 
-  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
 
-  res.json({ token, user });
+  res.json({ token, usershow });
 };
