1cec591f19a693d05d8d8cc8d454793e97a52cd3
Ivan Valenzuela <ivalenzuela@ntic.cl>
2025-11-17 17:17:44 -0300

    Rollback Middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,13 @@ import jwt from "jsonwebtoken";
 
 export const protect = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Sin token" });
 
-  next();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ message: "Token inválido" });
+  }
 };
